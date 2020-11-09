29db2dc43a2aa8204fb678e7d78e8e1e5c4d464e
Menghua <zoeaeen13@gmail.com>
2020-11-09 17:25:24 +0800

    finish hw1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,148 @@
-import logo from './logo.svg';
 import './App.css';
+import {Form, FormButtons} from "./components/FormItems";
+import {Wrapper, TodosWrapper} from "./components/Wrappers";
+import {TodoItem} from "./components/TodoItem";
+import { useState } from 'react';
 
+let id = 3
 function App() {
+  const [selectedType, setSelectType] = useState("All");
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      content: "七點半買早餐",
+      isDone: true,
+      isShowed: true,
+    },
+    {
+      id: 2,
+      content: "洗碗倒垃圾",
+      isDone: false,
+      isShowed: true,
+    }
+  ])
+
+  // 輸入框
+  function handleInputChnage(e) {
+    setValue(e.target.value);
+  }
+
+  // 新增
+  function handleAddTodo(e) {
+    e.preventDefault();
+    if (value !== "") {
+      setTodos([
+        {
+          id,
+          content: value,
+          isDone: false,
+          isShowed: selectedType !==  'Completed',
+        },
+        ...todos,
+      ])
+  
+      id += 1;
+      setValue("");
+    }
+  }
+
+  // 刪除功能
+  function handleDeleteTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  // 切換狀態
+  function handleToggleTodo(id) {
+    setTodos(todos.map((todo)=>{
+      if (todo.id !== id) return todo;
+      return {
+        ...todo,
+        isDone: !todo.isDone,
+      };
+    }))
+  }
+  
+  // 編輯功能
+  function handlgeEditTodo(e) {
+    setTodos(todos.map((todo)=>{
+      if (todo.id !== Number(e.target.id)) return todo;
+      return {
+        ...todo,
+        content: e.target.value,
+      };
+    }))
+  }
+
+  // 清空已完成
+  function handleClearTodos() {
+    setTodos(todos.filter((todo) => !todo.isDone))
+  }
+
+  // 設置篩選按鈕
+  function handleSelectType(e) {
+    const mode = e.target.id
+    setSelectType(mode);
+
+    switch (mode) {
+      case 'Completed':
+        setTodos(todos.map((todo)=>{
+          if (todo.isDone) {
+            return {
+              ...todo,
+              isShowed: true,
+            }
+          }
+          return {
+            ...todo,
+            isShowed: false,
+          };
+        }))
+        break;
+      case 'Incomplete':
+        setTodos(todos.map((todo)=>{
+          if (!todo.isDone) {
+            return {
+              ...todo,
+              isShowed: true,
+            }
+          }
+          return {
+            ...todo,
+            isShowed: false,
+          };
+        }))
+        break;
+      default:
+        setTodos(todos.map((todo)=>{
+          return {
+            ...todo,
+            isShowed: true,
+          };
+        }))
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Form value={value} handleInputChnage={handleInputChnage} handleAddTodo={handleAddTodo}/>
+        <FormButtons selectedType={selectedType} handleSelectType={handleSelectType} handleClearTodos={handleClearTodos} />
+        <TodosWrapper>
+          {todos.map((todo) => {
+            if (todo.isShowed) {
+              return <TodoItem
+              key={todo.id}
+              todo={todo}
+              handlgeEditTodo={handlgeEditTodo} 
+              handleToggleTodo={handleToggleTodo} 
+              handleDeleteTodo={handleDeleteTodo}
+            />
+            }
+          })}
+        </TodosWrapper>
+      </Wrapper>
     </div>
   );
 }
